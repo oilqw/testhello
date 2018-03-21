5e5a356dd7bd8cfdf7a7aadc770a8a7eff994d92
develop <develop@10.90.73.137>
2018-03-21 14:41:10 +0800

    1

--- src/main/java/com/test/testhello/SampleController.java
+++ src/main/java/com/test/testhello/SampleController.java
@@ -13,7 +13,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello Worldabc!";
     }
 
     public static void main(String[] args) throws Exception {
